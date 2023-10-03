c5a285c11b48660e288184fadefa1256e36e7854
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-10-02 18:09:18 -0700

    added my dependencies and added code for a basic header

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const React = () => {
+    return (
+        <header className="bg-gray-800 py-4">
+            <div className="container mx-auto">
+                <h1 className="text-white text-2x1 font-semibold"> Oswaldo Obadiah Felix Thompson</h1>
+                <p className="text-grey-300 text-md">Web Developer</p>
+                {/* going to add nav links in here */}
+            </div>
+        </header>
+    )
+}
