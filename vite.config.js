e6e45617a63dbc514562b405ee0cff01ab51886a
Oswaldo Felix Thompson <oswaldofelixthompson@Oswaldos-MacBook-Air.local>
2026-01-13 10:39:41 -0800

    updated portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import ( defineConfig ) from 'vite'
+import {defineConfig} from "vite";
 import react from '@vitejs/plugin-react'
 
-export defualt defineConfig({
+export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
